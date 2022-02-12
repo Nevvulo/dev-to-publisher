92f7f3d9def51f1da57c0e99f3239de5b29df1c5
Blake Ward <26913683+Nevvulo@users.noreply.github.com>
2022-02-13 02:07:25 +1100

    Add ability to update existing posts

--- src/main.ts
+++ src/main.ts
@@ -2,28 +2,53 @@ import * as core from "@actions/core";
 import axios from "axios";
 import { parseAsStringArray } from "./utils/parse";
 
-async function createPost(
-  apiKey: string,
-  title: string,
-  body: string,
-  draft: boolean,
-  tags: string[]
-) {
+interface PostOptions {
+  title: string;
+  description: string;
+  image: string;
+  body: string;
+  draft: boolean;
+  tags: string[];
+  originalUrl: string;
+}
+
+async function createPost(apiKey: string, options: PostOptions) {
+  const { title, body, description, image, originalUrl, tags, draft } = options;
+  const headers = { "api-key": apiKey };
   const published = !draft;
   const data = {
     article: {
       title,
+      description,
+      main_image: image,
       body_markdown: body,
+      canonical_url: originalUrl,
       published,
       tags,
     },
   };
 
-  const headers = {
-    "api-key": apiKey,
+  await axios.post("https://dev.to/api/articles", data, { headers });
+}
+
+async function updatePost(apiKey: string, id: string, options: PostOptions) {
+  const { title, body, description, image, originalUrl, tags, draft } = options;
+
+  const headers = { "api-key": apiKey };
+  const published = !draft;
+  const data = {
+    article: {
+      title,
+      description,
+      main_image: image,
+      body_markdown: body,
+      canonical_url: originalUrl,
+      published,
+      tags,
+    },
   };
 
-  await axios.post("https://dev.to/api/articles", data, { headers: headers });
+  await axios.put(`https://dev.to/api/articles/${id}`, data, { headers });
 }
 
 async function run() {
@@ -31,15 +56,33 @@ async function run() {
   if (title.length < 1) return fatalError("Title too short");
 
   const body = core.getInput("body", { required: true });
-  const isDraft = core.getBooleanInput("draft", { required: true });
+  const description = core.getInput("description");
+  const image = core.getInput("main_image");
+  const originalUrl = core.getInput("original_url");
+  const draft = core.getBooleanInput("draft", { required: true });
   const apiKey = core.getInput("api_key", { required: true });
+  const existingId = core.getInput("id");
 
   const tags = parseAsStringArray(core.getInput("tags")) || [];
   if (tags.length > 4) return fatalError("Cannot add more than 4 tags");
 
   try {
-    await createPost(apiKey, title, body, isDraft, tags);
+    const post = {
+      title,
+      description,
+      image,
+      body,
+      originalUrl,
+      draft,
+      tags,
+    };
+    if (existingId) {
+      await updatePost(apiKey, existingId, post);
+    } else {
+      await createPost(apiKey, post);
+    }
   } catch (e) {
+    console.debug(e);
     core.setFailed("Invalid API key provided");
     process.exit(1);
   }
